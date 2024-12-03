893f30ccf00635da82bc3b925ba301dec2cfaa82
shivu1919 <shivamthakur.ss129@gmail.com>
2024-12-03 18:58:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = React.createElement('h2',{className:"heading"},"This is a heading")
+const h1 = React.createElement('h1',{className:"sub"},"This is another heading")
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+
+root.render(h1)
